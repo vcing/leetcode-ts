5c144104263bd0008ae57ed1d647d7834be2fff1
vcing <2131287@qq.com>
2018-12-19 22:22:17 +0800

    update 26 ~28

---
+++ src/26.remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ *
+ * algorithms
+ * Easy (38.56%)
+ * Total Accepted:    473.7K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted array nums, remove the duplicates in-place such that each
+ * element appear only once and return the new length.
+ * 
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array in-place with O(1) extra memory.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Given nums = [1,1,2],
+ * 
+ * Your function should return length = 2, with the first two elements of nums
+ * being 1 and 2 respectively.
+ * 
+ * It doesn't matter what you leave beyond the returned length.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ * 
+ * Your function should return length = 5, with the first five elements of nums
+ * being modified to 0, 1, 2, 3, and 4 respectively.
+ * 
+ * It doesn't matter what values are set beyond the returned length.
+ * 
+ * 
+ * Clarification:
+ * 
+ * Confused why the returned value is an integer but your answer is an array?
+ * 
+ * Note that the input array is passed in by reference, which means
+ * modification to the input array will be known to the caller as well.
+ * 
+ * Internally you can think of this:
+ * 
+ * 
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ * 
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len
+ * elements.
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var removeDuplicates = function (nums: number[]) {
+//   for (let i = 0; nums[i] !== undefined; i++) {
+//     let count = 0
+//     let ii = i
+//     while (nums[ii] === nums[++ii]) count++
+//     nums.splice(i, count)
+//   }
+//   return nums.length
+// };
+
+var removeDuplicates = function (nums: number[]) {
+  let p1 = 0
+  let p2 = 1
+  let count = 1
+  while (nums[p2] !== undefined) {
+    if (nums[p1] === nums[p2]) {
+      p2++
+    } else {
+      p1++
+      nums[p1] = nums[p2]
+      p2++
+      count++
+    }
+  }
+  return count
+};
