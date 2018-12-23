0062e94075cdace98225b30ff2eaecec6775e0ae
vcing <2131287@qq.com>
2018-12-24 01:13:42 +0800

    update 36 39

---
+++ src/36.valid-sudoku.ts
@@ -0,0 +1,122 @@
+/*
+ * @lc app=leetcode id=36 lang=javascript
+ *
+ * [36] Valid Sudoku
+ *
+ * https://leetcode.com/problems/valid-sudoku/description/
+ *
+ * algorithms
+ * Medium (40.53%)
+ * Total Accepted:    198.1K
+ * Total Submissions: 486.7K
+ * Testcase Example:  '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
+ *
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be
+ * validated according to the following rules:
+ * 
+ * 
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without
+ * repetition.
+ * 
+ * 
+ * 
+ * A partially filled sudoku which is valid.
+ * 
+ * The Sudoku board could be partially filled, where empty cells are filled
+ * with the character '.'.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * [
+ * ⁠ ["5","3",".",".","7",".",".",".","."],
+ * ⁠ ["6",".",".","1","9","5",".",".","."],
+ * ⁠ [".","9","8",".",".",".",".","6","."],
+ * ⁠ ["8",".",".",".","6",".",".",".","3"],
+ * ⁠ ["4",".",".","8",".","3",".",".","1"],
+ * ⁠ ["7",".",".",".","2",".",".",".","6"],
+ * ⁠ [".","6",".",".",".",".","2","8","."],
+ * ⁠ [".",".",".","4","1","9",".",".","5"],
+ * ⁠ [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * [
+ * ["8","3",".",".","7",".",".",".","."],
+ * ["6",".",".","1","9","5",".",".","."],
+ * [".","9","8",".",".",".",".","6","."],
+ * ["8",".",".",".","6",".",".",".","3"],
+ * ["4",".",".","8",".","3",".",".","1"],
+ * ["7",".",".",".","2",".",".",".","6"],
+ * [".","6",".",".",".",".","2","8","."],
+ * [".",".",".","4","1","9",".",".","5"],
+ * [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner
+ * being 
+ * ⁠   modified to 8. Since there are two 8's in the top left 3x3 sub-box, it
+ * is invalid.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * A Sudoku board (partially filled) could be valid but is not necessarily
+ * solvable.
+ * Only the filled cells need to be validated according to the mentioned
+ * rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
+ * 
+ * 
+ */
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board: string[][]) {
+  let result = true
+  const check = (chars: string[]) => {
+    const digitals = chars.filter(c => c !== '.')
+    return digitals.length === new Set(digitals).size
+  }
+
+  const rowResult = board.every(check)
+  if (!rowResult) return false
+  const columnResult = board.reduce((prev, curr, index) => {
+    curr.forEach((c, i) => {
+      if (prev[i]) prev[i].push(c)
+      else prev[i] = [c]
+    })
+    return prev
+  }, [[]]).every(check)
+  if (!columnResult) return false
+
+  let blockResult = true
+  for (let i = 0; i < 9; i++) {
+    if (blockResult === false) return false
+    const column = i % 3
+    const row = Math.floor(i / 3)
+    const arr = []
+    for (let ii = 0; ii < 3; ii++) {
+      const y = row * 3 + ii
+      for (let iii = 0; iii < 3; iii++) {
+        const x = column * 3 + iii
+        arr.push(board[y][x])
+      }
+    }
+    blockResult = blockResult && check(arr)
+  }
+  return blockResult
+};
+
+// console.log(isValidSudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]))
---
+++ src/39.combination-sum.ts
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode id=39 lang=javascript
+ *
+ * [39] Combination Sum
+ *
+ * https://leetcode.com/problems/combination-sum/description/
+ *
+ * algorithms
+ * Medium (44.99%)
+ * Total Accepted:    280.2K
+ * Total Submissions: 619.8K
+ * Testcase Example:  '[2,3,6,7]\n7'
+ *
+ * Given a set of candidate numbers (candidates) (without duplicates) and a
+ * target number (target), find all unique combinations in candidates where the
+ * candidate numbers sums to target.
+ * 
+ * The same repeated number may be chosen from candidates unlimited number of
+ * times.
+ * 
+ * Note:
+ * 
+ * 
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ * ⁠ [7],
+ * ⁠ [2,2,3]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ * 
+ * 
+ */
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates: number[], target: number) {
+  const result = []
+  candidates = candidates.sort((a, b) => a - b)
+  if (candidates[candidates.length - 1] === target) {
+    result.push([candidates[candidates.length - 1]])
+    candidates.splice(candidates.length - 1)
+  } else if (candidates[candidates.length - 1] > target) {
+    const last = candidates.findIndex(x => x > target)
+    candidates.splice(last)
+  }
+  // const boundary = Math.floor(target / candidates[0])
+  // if (target % candidates[0] === 0) {
+  //   result.push(new Array(target / candidates[0]).fill(candidates[0]))
+  // } else {
+  //   for (let i = 1; i < candidates.length; i++) {
+  //     if ((boundary - 1) * candidates[0] + candidates[i] === target)
+  //       result.push([...new Array(boundary - 1).fill(candidates[0]), candidates[i]])
+  //     else if ((boundary - 1) * candidates[0] + candidates[i] > target) break
+  //   }
+  // }
+
+  for (let i = 1; i <= Math.floor(target / candidates[0]); i++)
+    arrayLoop([], i, candidates, target, result)
+  return result
+};
+
+const arrayLoop = (current: number[], length: number, arr: number[], target: number, result: number[][]) => {
+  if (length > 1) {
+    let subArr = arr
+    if (current.length > 0) subArr = arr.slice(arr.indexOf(current[current.length - 1]))
+    const start = arr.findIndex(n => n === current[current.length - 1])
+    for (let i = start; i < arr.length; i++) arrayLoop([...current, arr[i]], length - 1, subArr, target, result)
+  } else {
+    const fixed = current.reduce((prev, curr) => prev + curr, 0)
+    const start = arr.findIndex(n => n === current[current.length - 1])
+    for (let i = start; i < arr.length; i++) {
+      if (arr[i] + fixed === target) result.push([...current, arr[i]])
+      else if (arr[i] + fixed > target) break
+    }
+  }
+}
+
+// console.log(combinationSum([2, 3, 5], 8))
+// console.log(combinationSum([2, 3, 8, 4], 6))
+// console.log(combinationSum([1, 2], 1))
+// console.log(combinationSum([3, 12, 9, 11, 6, 7, 8, 5, 4], 15))
