350336a93abbccb6b950fa9328cccafc1d1ea735
vcing <2131287@qq.com>
2018-12-30 21:48:04 +0800

    update 43

--- src/43.multiply-strings.ts
+++ src/43.multiply-strings.ts
@@ -45,5 +45,36 @@
  * @return {string}
  */
 var multiply = function (num1: string, num2: string) {
-
+  if (num1 === '0' || num2 === '0') return '0'
+  const num1Arr = num1.split('')
+  const num2Arr = num2.split('')
+  const sum: number[] = []
+  num1Arr.map((n, i) => {
+    const n1 = parseInt(n)
+    num2Arr.map((nn, j) => {
+      const n2 = parseInt(nn)
+      // console.log(n1, n2)
+      if (sum[i + j] === undefined) sum[i + j] = n1 * n2
+      else sum[i + j] += n1 * n2
+    }, 0)
+  }, 0)
+  let result = ''
+  // console.log(sum)
+  for (let i = sum.length - 1; i >= 0; i--) {
+    const n = sum[i]
+    if (n > 9) {
+      const carry = Math.floor(n / 10)
+      if (i === 0) {
+        result = n + result
+      } else {
+        if (sum[i - 1]) sum[i - 1] += carry
+        else sum[i - 1] = carry
+        result = n % 10 + result
+      }
+    } else result = n + result
+  }
+  return result
 };
+
+// console.log(multiply("123456789", "987654321"))
+// console.log(multiply("123456789", "987654321"))
