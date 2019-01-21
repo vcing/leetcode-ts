500a98eecbd50dca96f3a76e1cc78257312f0ecc
vcing <2131287@qq.com>
2019-01-21 22:53:25 +0800

    update 73

---
+++ dist/73.set-matrix-zeroes.js
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=73 lang=javascript
+ *
+ * [73] Set Matrix Zeroes
+ *
+ * https://leetcode.com/problems/set-matrix-zeroes/description/
+ *
+ * algorithms
+ * Medium (38.18%)
+ * Total Accepted:    182.4K
+ * Total Submissions: 472.8K
+ * Testcase Example:  '[[1,1,1],[1,0,1],[1,1,1]]'
+ *
+ * Given a m x n matrix, if an element is 0, set its entire row and column to
+ * 0. Do it in-place.
+ *
+ * Example 1:
+ *
+ *
+ * Input:
+ * [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * Output:
+ * [
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]
+ * ]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input:
+ * [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * Output:
+ * [
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
+ *
+ *
+ * Follow up:
+ *
+ *
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best
+ * solution.
+ * Could you devise a constant space solution?
+ *
+ *
+ */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+    const zeros = [];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === 0)
+                zeros.push([i, j]);
+        }
+    }
+    zeros.map(([y, x]) => {
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][x] = 0;
+        }
+        for (let i = 0; i < matrix[0].length; i++) {
+            matrix[y][i] = 0;
+        }
+    });
+    return matrix;
+};
+// console.log(setZeroes([
+//   [0, 1, 2, 0],
+//   [3, 4, 5, 2],
+//   [1, 3, 1, 5]
+// ]))
