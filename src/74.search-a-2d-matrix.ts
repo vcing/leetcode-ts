762c447705c4e7803c0bc3c4b8e65398df0a8ee9
vcing <2131287@qq.com>
2019-01-23 00:10:30 +0800

    update 74

--- src/74.search-a-2d-matrix.ts
+++ src/74.search-a-2d-matrix.ts
@@ -51,19 +51,34 @@
  * @param {number} target
  * @return {boolean}
  */
+// var searchMatrix = function (matrix: number[][], target: number): boolean {
+//   if (matrix.length === 0 || matrix[0].length === 0) return false
+//   if (matrix.length === 1) return matrix[0].indexOf(target) !== -1
+//   const y = Math.floor(matrix.length / 2)
+//   const firstNumber = matrix[y][0]
+//   if (firstNumber === target) return true
+//   else if (matrix[y].indexOf(target) !== -1) return true
+//   else if (firstNumber > target) return searchMatrix(matrix.slice(0, y), target)
+//   else if (firstNumber < target) return searchMatrix(matrix.slice(y + 1), target)
+// };
+
 var searchMatrix = function (matrix: number[][], target: number): boolean {
   if (matrix.length === 0 || matrix[0].length === 0) return false
   if (matrix.length === 1) return matrix[0].indexOf(target) !== -1
-  const y = Math.floor(matrix.length / 2)
-  const firstNumber = matrix[y][0]
-  if (firstNumber === target) return true
-  else if (matrix[y].indexOf(target) !== -1) return true
-  else if (firstNumber > target) return searchMatrix(matrix.slice(0, y), target)
-  else if (firstNumber < target) return searchMatrix(matrix.slice(y + 1), target)
+  let left = 0
+  let right = matrix.length - 1
+  while (right > left) {
+    const binary = Math.floor((left + right) / 2)
+    if (matrix[binary][0] === target) return true
+    else if (matrix[binary][0] < target && matrix[binary + 1][0] > target) return matrix[binary].indexOf(target) !== -1
+    else if (matrix[binary][0] > target) right = binary - 1
+    else if (matrix[binary][0] < target) left = binary + 1
+  }
+  return matrix[left].indexOf(target) !== -1
 };
 
 // console.log(searchMatrix([
 //   [1, 3, 5, 7],
 //   [10, 11, 16, 20],
 //   [23, 30, 34, 50]
-// ], 31))
+// ], 15))
