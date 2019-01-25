32fb14992488d255a7e1c4a844afe84b3d735701
vcing <2131287@qq.com>
2019-01-25 17:55:38 +0800

    update 78

---
+++ dist/78.subsets.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=78 lang=javascript
+ *
+ * [78] Subsets
+ *
+ * https://leetcode.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (49.17%)
+ * Total Accepted:    316.7K
+ * Total Submissions: 631.7K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a set of distinct integers, nums, return all possible subsets (the
+ * power set).
+ *
+ * Note: The solution set must not contain duplicate subsets.
+ *
+ * Example:
+ *
+ *
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+    const result = [[]];
+    const backTracking = (current, length, start) => {
+        if (current.length === length)
+            return result.push(current);
+        for (let i = start; i < nums.length; i++)
+            backTracking(current.concat(nums[i]), length, i + 1);
+    };
+    for (let i = 1; i <= nums.length; i++)
+        backTracking([], i, 0);
+    return result;
+};
+// var subsets = function(s) {
+//   if (s.length == 0) {
+//       return [[]]
+//   } else {
+//       let rest = subsets(s.slice(1))  
+//       let newSets = rest.map(         
+//           (set) => [s[0], ...set]
+//       )
+//       return [...rest, ...newSets]    
+//   }
+// };
+console.log(subsets([1, 2, 3]));
+// console.log(subsets([4, 2, 3, 4, 5, 6]))
