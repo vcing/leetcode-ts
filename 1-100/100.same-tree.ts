630697fedc61bf5034b63c8628c18c2145b99ba6
vcing <2131287@qq.com>
2019-03-25 14:27:23 +0800

    update 100

--- 1-100/100.same-tree.ts
+++ 1-100/100.same-tree.ts
@@ -67,5 +67,8 @@
  * @return {boolean}
  */
 var isSameTree = function (p: TreeNode, q: TreeNode) {
-
+  if (!p && !q) return true
+  if (!p || !q) return false
+  if (p.val === q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  else return false
 };
