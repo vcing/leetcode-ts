38e9be02d9e27a2bd279db6084a9c800b1e35eb6
vcing <2131287@qq.com>
2019-05-24 23:50:53 +0800

    update 105

--- src/105.construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ src/105.construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -12,25 +12,25 @@
  * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
  *
  * Given preorder and inorder traversal of a tree, construct the binary tree.
- *
+ * 
  * Note:
  * You may assume that duplicates do not exist in the tree.
- *
+ * 
  * For example, given
- *
- *
+ * 
+ * 
  * preorder = [3,9,20,15,7]
  * inorder = [9,3,15,20,7]
- *
+ * 
  * Return the following binary tree:
- *
- *
+ * 
+ * 
  * ⁠   3
  * ⁠  / \
  * ⁠ 9  20
  * ⁠   /  \
  * ⁠  15   7
- *
+ * 
  */
 /**
  * Definition for a binary tree node.
@@ -44,18 +44,21 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function (preorder, inorder) {
-    function helper(p1, p2, i1, i2) {
-        if (p1 > p2 || i1 > i2)
-            return null; // sanity check
-        const value = preorder[p1]; // get the root value
-        const index = inorder.indexOf(value); // get inorder position
-        const nLeft = index - i1; // count nodes in left subtree
-        const root = { val: value, left: null, right: null }; // build the root node
-        // build the left and right subtrees recursively
-        root.left = helper(p1 + 1, p1 + nLeft, i1, index - 1);
-        root.right = helper(p1 + nLeft + 1, p2, index + 1, i2);
-        return root;
-    }
-    return helper(0, preorder.length - 1, 0, inorder.length - 1);
+var buildTree = function (preorder: number[], inorder: number[]) {
+  function helper(p1: number, p2: number, i1: number, i2: number) {
+    if (p1 > p2 || i1 > i2) return null; // sanity check
+
+    const value = preorder[p1]           // get the root value
+    const index = inorder.indexOf(value) // get inorder position
+    const nLeft = index - i1             // count nodes in left subtree
+    const root: TreeNode = { val: value, left: null, right: null }    // build the root node
+
+    // build the left and right subtrees recursively
+    root.left = helper(p1 + 1, p1 + nLeft, i1, index - 1);
+    root.right = helper(p1 + nLeft + 1, p2, index + 1, i2);
+
+    return root;
+  }
+
+  return helper(0, preorder.length - 1, 0, inorder.length - 1);
 };
