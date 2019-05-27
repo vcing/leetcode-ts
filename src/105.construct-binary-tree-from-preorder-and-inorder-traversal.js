9a95e91723d303f9b91f9f747cd5fe8b95a1db14
vcing <2131287@qq.com>
2019-05-27 18:17:59 +0800

    update 105

--- src/105.construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ src/105.construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -45,17 +45,9 @@
  * @return {TreeNode}
  */
 var buildTree = function (preorder, inorder) {
-    function helper(p1, p2, i1, i2) {
-        if (p1 > p2 || i1 > i2)
-            return null; // sanity check
-        const value = preorder[p1]; // get the root value
-        const index = inorder.indexOf(value); // get inorder position
-        const nLeft = index - i1; // count nodes in left subtree
-        const root = { val: value, left: null, right: null }; // build the root node
-        // build the left and right subtrees recursively
-        root.left = helper(p1 + 1, p1 + nLeft, i1, index - 1);
-        root.right = helper(p1 + nLeft + 1, p2, index + 1, i2);
-        return root;
-    }
-    return helper(0, preorder.length - 1, 0, inorder.length - 1);
+    if (preorder.length === 0 || preorder.length !== inorder.length)
+        return null;
+    const val = preorder[0];
+    const valIndex = inorder.indexOf(val);
+    return { val, left: buildTree(preorder.slice(1, 1 + valIndex), inorder.slice(0, valIndex)), right: buildTree(preorder.slice(1 + valIndex), inorder.slice(valIndex + 1)) };
 };
