de2abda56437a86645cbbadf44720e9598210309
vcing <2131287@qq.com>
2019-07-16 10:41:28 +0800

    update 129

---
+++ src/129.sum-root-to-leaf-numbers.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=129 lang=javascript
+ *
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (42.52%)
+ * Total Accepted:    191.5K
+ * Total Submissions: 445.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ *
+ * An example is the root-to-leaf path 1->2->3 which represents the number
+ * 123.
+ *
+ * Find the total sum of all root-to-leaf numbers.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ *
+ * Input: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ *
+ * Example 2:
+ *
+ *
+ * Input: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root) {
+    if (!root)
+        return 0;
+    const helper = (root) => {
+        if (!root.left && !root.right)
+            return [root.val.toString()];
+        const left = root.left ? helper(root.left) : [];
+        const right = root.right ? helper(root.right) : [];
+        return left.concat(right).map(num => root.val + '' + num);
+    };
+    return helper(root).reduce((prev, curr) => prev + parseInt(curr), 0);
+};
