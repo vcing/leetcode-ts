5b577683a89132ae763f3b854242a52d0cada796
vcing <2131287@qq.com>
2019-08-07 17:32:00 +0800

    update 141

---
+++ src/141.linked-list-cycle.js
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (36.95%)
+ * Total Accepted:    435.3K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Can you solve it using O(1) (i.e. constant) memory?
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+// var hasCycle = function(head: ListNode) {
+//   if(!head) return false
+//   let current = head
+//   const cache: ListNode[] = []
+//   while(current.next) {
+//     if(cache.indexOf(current) !== -1) return true
+//     cache.push(current)
+//     current = current.next
+//   }
+//   return false
+// };
+var hasCycle = function (head) {
+    if (!head)
+        return false;
+    let current = head;
+    while (current.next) {
+        if (current.flag)
+            return ture;
+        current.flag = ture;
+        current = current.next;
+    }
+    return false;
+};
